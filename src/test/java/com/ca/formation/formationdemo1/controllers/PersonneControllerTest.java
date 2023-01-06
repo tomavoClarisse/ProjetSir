dd73361b1da49ac4d5b7cc3963941173e8f5fbf3
tomavo <tomavoclarisse@gmail.com>
2023-01-06 10:11:22 +0000

    update

--- src/test/java/com/ca/formation/formationdemo1/controllers/PersonneControllerTest.java
+++ src/test/java/com/ca/formation/formationdemo1/controllers/PersonneControllerTest.java
@@ -54,22 +54,6 @@ public class PersonneControllerTest {
     private String tokenRequest;
 
     @Test
-    @WithMockUser(username = "clara@formation.sn", password = "Passer@123", authorities = { "ADMIN" })
-    public void byeTest() {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + tokenRequest);
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-
-        ResponseEntity<String> response = this.restTemplate.exchange("http://localhost:8082"+"/api/v2/personnes/bye",
-                HttpMethod.GET, entity, String.class);
-
-        //System.out.println("+++++++++++++++++++++" +response.() +" ++++++++++");
-        assertEquals(response.getBody(), "Bye bye");
-
-    }
-
-    @Test
     @WithMockUser(username = "michel@formation.sn", password = "Passer@123", authorities = { "READ" })
     public void getPersonnes() throws Exception {
         RequestBuilder requestBuilder = MockMvcRequestBuilders
